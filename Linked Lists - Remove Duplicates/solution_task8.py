10db4f4a5dd60a2c9edd723e9eea09355f2e82e2
Korch195 <korchevyi.pn@ucu.edu.ua>
2024-04-08 17:42:04 +0300

    confirmation + solution

--- Linked Lists - Remove Duplicates/solution_task8.py
+++ Linked Lists - Remove Duplicates/solution_task8.py
@@ -4,4 +4,14 @@ class Node(object):
         self.next = None
 
 def remove_duplicates(head):
-    ...
+    if head is None:
+        return None
+    
+    current = head
+    while current.next is not None:
+        if current.data == current.next.data:
+            current.next = current.next.next
+        else:
+            current = current.next
+    
+    return head
